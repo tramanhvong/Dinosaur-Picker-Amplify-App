b905d24f3a8b36c95d02919d0e64ba81d84c0f66
Tram Anh Vong <91495934+tramanhvong@users.noreply.github.com>
2025-10-24 18:35:23 +0700

    Improve renderDinosaur function and comments
    Refactor renderDinosaur function for clarity and consistency.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+/**Home page for the app with main layout**/
 
+// Imports
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -14,31 +16,42 @@ Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
+/** Main function
+1. Create a State object to store the month item.
+2. Define renderDinosaur function that takes an argument 'month' of type String, and render the corresponding dinosaur.
+3. Define return layout.
+**/
 export default function App() {
 
   const [month, setMonth] = useState("january");
 
   const renderDinosaur = (month: String) => {
-    month = month.toLowerCase();
+    month = month.toLowerCase(); // lower case to match name of files
     const container = document.getElementsByClassName("dinosaurPlaceholder")[0];
-    const root = ReactDOM.createRoot(container)
-    root.render(
-      <div className="dinosaur">
-        <img src={`/photos/${month}.jpg`} className="dinosaurImg" alt="dinosaur" />
+    const root = ReactDOM.createRoot(container) 
+    root.render( // case match month to name of file
+      <div className="dinosaur"> 
+        <img src={`/photos/${month}.jpg`} className="dinosaurImg" alt="dinosaur" /> 
       </div>
     );
   }
 
   return (
+    /** Return method:
+    1. Header part containing title and credits.
+    2. Insert background music.
+    3. Drop list to select month.
+    4. Button and placeholder for the dinosaur output. Button triggers renderDinosaur on click.
+    **/
     <main className="App">
       <header className="header">
         <h1>Which Dinosaur are you according to your birth month</h1>
         <h2>Credits to @crankbookscience on Instagram</h2>
       </header>
 
-      <div className="body">
+      <div className="body"> 
         <audio controls autoPlay loop>
-          <source src="/audio/yoshi.mp3" type="audio/mpeg" />
+          <source src="/audio/yoshi.mp3" type="audio/mpeg" /> 
         </audio>
         <div className="selectMonth">
           <label htmlFor="month" className="question">Select your birth month: </label>
